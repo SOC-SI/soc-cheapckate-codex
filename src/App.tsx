7fb1e94dbd02eeb306c95311ff539d364e0d15bd
Tim Prapotnik <tim.prapotnik@student.um.si>
2023-04-19 16:40:16 +0200

    Added link and exmaple router to board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import styles from './App.module.scss';
+import { Link } from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.App}>
+            <Link to="/login">Login</Link>
         </div>
     );
 }
